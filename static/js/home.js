4ac9d6f6cfc5fc56a02127f0230246c2197ca076
les-bres <les.bres.2@gmail.com>
2015-05-19 22:24:49 -0400

    changed up the links

--- static/js/home.js
+++ static/js/home.js
@@ -1,29 +1,115 @@
 console.log("HELLO");
+
 var App = new Marionette.Application();
+
 App.addRegions({
     firstRegion: "#first-region",
     secondRegion: "#second-region"
 });
+
 App.on("start", function(){
     console.log("STARTING");
+
     var linkView = new App.LinkView({model:l1});
-    //console.log(linkView);
+    console.log(linkView);
     //App.secondRegion.show(linkView);
+
     var linksView = new App.LinksView({collection:c});
-    console.log('yo');
     App.firstRegion.show(linksView);
-    console.log(linksView);
+
     Backbone.history.start();
 });
+
+
 App.LinkView = Marionette.ItemView.extend({
     template: "#link-template",
     tagName : "div",
     className: "panel panel-default",
     modelEvents: {
-	"change":function(){
-	    this.render();
-	}}
+    "change":function(){
+        this.render();
+    }}
 });
+
+
+App.LinksView = Marionette.CollectionView.extend({
+    childView: App.LinkView,
+    tagName: "div",
+    className: "panel-group",
+    
+    attributes: {
+        "id":"accordion",
+        "role":"tablist",
+        "aria-multiselectable":"true"
+    }
+});
+
+var Link = Backbone.Model.extend();
+var Links = Backbone.Collection.extend({
+    model:Link,
+    comparator:"name"
+});
+
+var l1 = new Link({title:"first article",date:"11/07/97",url:"meow"});
+var l2 = new Link({title:"second article",date:"11/08/98",url:"woof"});
+
+var c = new Links([l1,l2]);
+
+App.start();
+/*
+console.log("HELLO");
+
+var App = new Marionette.Application();
+
+App.addRegions({
+    firstRegion: "#first-region",
+    secondRegion: "#second-region"
+});
+
+App.on("start", function(){
+    console.log("STARTING");
+    var linkView = new App.LinkView({model:l1});
+    //console.log(linkView);
+    //App.secondRegion.show(linkView);
+     App.firstRegion.show(LinkView);
+    var linksView = new App.LinksView({collection:c});
+    console.log('yo');
+   
+    console.log(linksView);
+    Backbone.history.start();
+});
+
+App.LinkView = Marionette.ItemView.extend({
+    template: "#link-template",
+    tagName : "div",
+    className: "panel panel-default",
+    modelEvents: {
+    "change":function(){
+        this.render();
+    }}
+});
+
+App.LinksView = Marionette.CollectionView.extend({
+    childView: App.LinkView,
+    tagName: "div",
+    className: "panel-group"
+
+});
+
+var Link = Backbone.Model.extend();
+var Links = Backbone.Collection.extend({
+    model:Link,
+    comparator:"name"
+});
+
+var l1 = new Link({title:"first article",date:"11/07/97",url:"meow.com"});
+var l2 = new Link({title:"second article",date:"11/08/98",url:"woof.com"});
+
+var c = new Links([l1,l2]);
+
+App.start();
+/*
+
 App.LinksView = Marionette.CollectionView.extend({
     childView: App.LinkView,
     tagName: "div",
@@ -34,6 +120,7 @@ App.LinksView = Marionette.CollectionView.extend({
 //	"aria-multiselectable":"true"
   //  }
 });
+
 var Link = Backbone.Model.extend();
 var Links = Backbone.Collection.extend({
     model:Link,
@@ -43,4 +130,4 @@ var l1 = new Link({title:"first article",date:"11/07/97",url:"meow.com"});
 var l2 = new Link({title:"second article",date:"11/08/98",url:"woof.com"});
 var c = new Links([l1,l2]);
 console.log(c);
-App.start();
+App.start();*/
