c09a9a56845d4f57ee4a2cf8ea8da0165793217e
mchaiken <mirandachaiken@gmail.com>
2015-06-14 21:32:30 -0400

    changes

--- summarizeChrome/popup.js
+++ summarizeChrome/popup.js
@@ -2,7 +2,7 @@ function popup(url) {
     var w = window.open(url, "Summarize!", "width=500, height=500");
     return w
 }
-popup("http://google.com");
+//popup("http://google.com");
 
 var url = window.location.href;
 
@@ -10,7 +10,7 @@ while (url.indexOf("/") != -1){
     url=url.replace("/","%9l");
 }
 
-var win = popup("localhost:5000/summary/"+url);
+var win = popup("http://104.236.53.73/summary/"+url);
 //win.location.href= url;
 console.log(win)
 //win.location.onLoad = win.location.reload();
